371639494b066babbf662524af20935c1232a0f8
Dorian Kcira <dkcira@gmail.com>
2019-06-26 23:08:19 -0700

    aliases

---
+++ aliases.sh
@@ -0,0 +1,24 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: aliases.sh
+# 
+#         USAGE: ./aliases.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: YOUR NAME (), 
+#  ORGANIZATION: 
+#       CREATED: 06/26/2019 23:06
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+
+# dockerdaemon on macos
+alias dockerdaemon="bash --login '/Applications/Docker/Docker Quickstart Terminal.app/Contents/Resources/Scripts/start.sh'"
+
