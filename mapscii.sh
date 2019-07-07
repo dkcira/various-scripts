552c73d9fa76f8e3544876e102181b676da7d485
Dorian Kcira <dkcira@gmail.com>
2019-07-07 15:52:04 -0700

    mapscii

---
+++ mapscii.sh
@@ -0,0 +1,23 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: mapscii.sh
+# 
+#         USAGE: ./mapscii.sh 
+# 
+#   DESCRIPTION:  ascii map on the terminal
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: YOUR NAME (), 
+#  ORGANIZATION: 
+#       CREATED: 07/07/2019 15:49
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+
+telnet mapscii.me
+
